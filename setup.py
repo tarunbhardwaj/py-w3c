46027a99c99d450fd2be8b8be23abe3b22b5c16a
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2014-12-10 08:25:06 +0530

    Added w3c-validator as command line

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='w3c-validator-cli',
+    version='1.0',
+    description='w3c validator cli',
+    author='Tarun Bhardwaj',
+    author_email='tarun.bhardwaj@openlabs.co.in',
+    url='https://github.com/tarunbhardwaj/py-w3c',
+    scripts=['w3c-validator'],
+    license='MIT'
+)
